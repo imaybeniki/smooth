968a06faf105deaa0296c1ee6826d03f8615ee25
May, Nicole [USA] <May_Nicole@bah.com>
2017-07-06 16:48:18 -0400

    Java
    Test

--- tester.java
+++ tester.java
@@ -1,5 +1,7 @@
-// Send a message
-System.out.println("Sending a message to MyQueue.\n");
-sqs.sendMessage(new SendMessageRequest()
-    .withQueueUrl("https://sqs.us-east-1.amazonaws.com/258476513244/Temperature")
-    .withMessageBody("This is my message text."));
+public class tester{
+	public static void main(String[] args){
+		// Send a message
+		System.out.println("Sending a message to MyQueue.\n");
+		sqs.sendMessage(new SendMessageRequest().withQueueUrl("https://sqs.us-east-1.amazonaws.com/258476513244/Temperature").withMessageBody("This is my message text."));
+	}
+}
