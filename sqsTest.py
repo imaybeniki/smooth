f90b6ea7e006d21d169c34597ff278ef30b5adea
May, Nicole [USA] <May_Nicole@bah.com>
2017-07-06 15:58:40 -0400

    Region
    Y

--- sqsTest.py
+++ sqsTest.py
@@ -3,8 +3,7 @@ import os
 import glob
 import time
 
-sqs = boto3.resource('sqs')
-kms = boto3.client('kms', region_name='us-east-1')
+sqs = boto3.resource('sqs', region_name='us-east-1')
 
 def sent_to_q(message):
     sqs = boto3.resource('sqs')
