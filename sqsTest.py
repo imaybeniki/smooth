10d5529b02faeafe114a7e58700238799b70886d
May, Nicole [USA] <May_Nicole@bah.com>
2017-07-06 17:19:36 -0400

    GahhhhDamn
    Really though

--- sqsTest.py
+++ sqsTest.py
@@ -2,6 +2,7 @@ import boto3
 import os
 import glob
 import time
+from boto3.session import Session
 
 conf = { 
    "sqs-access-key": "AKIAJUYQR43YRT6APTJQ", 
@@ -10,12 +11,27 @@ conf = {
    "sqs-region": "us-east-1", 
    "sqs-path": "sqssend" 
  } 
+ 
+# Create the Boto3 Session
+session = Session(
+    aws_access_key_id='AKIAJUYQR43YRT6APTJQ',
+    aws_secret_access_key='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',
+    region_name='us-east-1',
+)
+client = session.client('sqs')
 
+# Get the Queue URL
+response = client.get_queue_url(
+    QueueName='Temperature' # Or the name of your SQS queue
+)
+url = response['https://sqs.us-east-1.amazonaws.com/258476513244/Temperature']
 
-def sent_to_q(message):
-    sqs = boto3.resource('sqs')
-    queue = sqs.get_queue_by_name(QueueName='Temperature')
-    response = queue.send_message(MessageBody = message)
+message = 'Hello world!'
+response = client.send_message(
+    QueueUrl=url,
+    MessageBody=message,
+    DelaySeconds=0,
+)
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
