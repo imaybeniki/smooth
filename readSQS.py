3fa16b4101bc68e4b9879b3e0280dee3b63a1c20
May, Nicole [USA] <May_Nicole@bah.com>
2017-07-07 14:32:34 -0400

    The hope is alive
    Blah

--- readSQS.py
+++ readSQS.py
@@ -1,37 +1,27 @@
 import boto3
-import json
+import time
+from boto3.session import Session
+ 
+# Create the Boto3 Session
+session = Session(
+    aws_access_key_id='AKIAJUYQR43YRT6APTJQ',
+    aws_secret_access_key='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',
+    region_name='us-east-1',
+)
+client = session.client('sqs')
 
-region_name = 'us-east-1'
-queue_name = 'Temperature'
-max_queue_messages = 10
-message_bodies = []
-aws_access_key_id = 'AKIAJUYQR43YRT6APTJQ'
-aws_secret_access_key = '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'
-sqs = boto3.resource('sqs', region_name=region_name,
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key)
-queue = sqs.get_queue_by_name(QueueName=queue_name)
+# Get the Queue URL
+response = client.get_queue_url(
+    QueueName='Temperature' 
+)
+
+
 while True:
-    messages_to_delete = []
-    for message in queue.receive_messages(
-            MaxNumberOfMessages=max_queue_messages)
-        # process message body
-        body = json.loads(message.body)
-        message_bodies.append(body)
-		print(body)
-        # add message to delete
-        messages_to_delete.append({
-            'Id': message.message_id,
-            'ReceiptHandle': message.receipt_handle
-        })
-
-    # if you don't receive any notifications the
-    # messages_to_delete list will be empty
-    if len(messages_to_delete) == 0:
-        break
-    # delete messages to remove them from SQS queue
-    # handle any errors
-    else:
-        delete_response = queue.delete_messages(
-                Entries=messages_to_delete)
+   response = client.receive_message(
+    QueueUrl='https://sqs.us-east-1.amazonaws.com/258476513244/Temperature',
+    DelaySeconds=0,
+   )
+   print(response)
+   time.sleep(5)
+   
    
